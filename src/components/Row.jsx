5ee74ec5d94b7bff81497505833af4076be5679d
Louisebfraser <louise.b.fraser@vanderbilt.edu>
2025-10-10 00:26:18 -0400

    Full functionality

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,12 +1,28 @@
-import Letter from "./Letter";
 import "./Comp.css";
 
-export default function Row({ word }) {
-  return (
-    <div className="row">
-      {word.split("").map((char, i) => (
-        <Letter char={char} key={i} />
-      ))}
-    </div>
-  );
+export default function Row({ guess, isFinal, solution }) {
+  const block = [];
+
+  for (let i = 0; i < 5; i++) {
+    let className = "letter";
+    const char = guess[i];
+
+    if (isFinal) {
+      if (char === solution[i]) {
+        className += " correct";
+      } else if (solution.includes(char)) {
+        className += " close";
+      } else {
+        className += " wrong";
+      }
+    }
+
+    block.push(
+      <div key={i} className={className}>
+        <h2>{char}</h2>
+      </div>
+    );
+  }
+
+  return <div className="row">{block} </div>;
 }
